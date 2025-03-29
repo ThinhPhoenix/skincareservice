d23f6d4190e33a1fb90884fd7bf0a7615c69e080
ThinhPhoenix <laithinh1310@gmail.com>
2025-03-29 14:42:52 +0700

    chore: crud user

--- SkinCareDAO/UserDAO.cs
+++ SkinCareDAO/UserDAO.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SkinCareBussinessObject;
+using SkinCareDAO.Utils;
 
 namespace SkinCareDAO
 {
@@ -33,11 +34,57 @@ namespace SkinCareDAO
         public User? SignIn(string email, string password)
         {
             return _dbContext.Users.SingleOrDefault(
-                u => 
+                u =>
                 u.Email.ToLower().Equals(email.ToLower())
                 &&
-                u.Password.Equals(password)
+                u.Password.Equals(MyUtils.Decrypt(password))
             );
         }
+
+        public User? GetOne(string id)
+        {
+            return _dbContext.Users
+                .SingleOrDefault(a => a.Id.Equals(id));
+        }
+
+        public List<User> GetAll()
+        {
+            return _dbContext.Users
+                .ToList();
+        }
+
+        public void SignUp(User a)
+        {
+            User cur = GetOne(a.Id);
+            if (cur != null)
+            {
+                throw new Exception();
+            }
+            a.Password = MyUtils.Encrypt(a.Password);
+            _dbContext.Users.Add(a);
+            _dbContext.SaveChanges();
+        }
+
+        public void Update(User a)
+        {
+            User cur = GetOne(a.Id);
+            if (cur == null)
+            {
+                throw new Exception();
+            }
+            _dbContext.Entry(cur).CurrentValues.SetValues(a);
+            _dbContext.SaveChanges();
+        }
+
+        public void Delete(string id)
+        {
+            User cur = GetOne(id);
+            if (cur != null)
+            {
+                _dbContext.Users.Remove(cur);
+                _dbContext.SaveChanges(); // Delete the object
+            }
+        }
+
     }
 }
