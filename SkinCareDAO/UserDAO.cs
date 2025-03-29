c637d8666da65fa527670444fcb71cc3e7e6ea69
ThinhPhoenix <laithinh1310@gmail.com>
2025-03-29 14:25:38 +0700

    signin

---
+++ SkinCareDAO/UserDAO.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SkinCareBussinessObject;
+
+namespace SkinCareDAO
+{
+    public class UserDAO
+    {
+
+        private SkinCareDBContext _dbContext;
+        private static UserDAO instance;
+
+        public UserDAO()
+        {
+            _dbContext = new SkinCareDBContext();
+        }
+
+        public static UserDAO Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new UserDAO();
+                }
+                return instance;
+            }
+        }
+
+        public User? SignIn(string email, string password)
+        {
+            return _dbContext.Users.SingleOrDefault(
+                u => 
+                u.Email.ToLower().Equals(email.ToLower())
+                &&
+                u.Password.Equals(password)
+            );
+        }
+    }
+}
