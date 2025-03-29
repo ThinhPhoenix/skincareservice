cc5d19e5169e101cecc19dccf8bc1dc534bcfcbc
ThinhPhoenix <laithinh1310@gmail.com>
2025-03-29 21:45:49 +0700

    chore: staff repository

---
+++ SkinCareRepository/IStaffRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SkinCareBussinessObject;
+using SkinCareDAO;
+
+namespace SkinCareRepository
+{
+    public interface IStaffRepository
+    {
+        public Staff GetOne(string id);
+
+        public List<Staff> GetAll();
+
+        public void Add(Staff a);
+
+        public void Update(Staff a);
+
+        public void Delete(string id);
+    }
+}
