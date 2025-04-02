1a9113634ede4f8ec4cb53a22abdc772fb77bfa9
Tnhaan20 <nhancd909@gmail.com>
2025-04-02 11:39:33 +0700

    feat: appointment dao crud

--- SkinCareDAO/AppointmentDAO.cs
+++ SkinCareDAO/AppointmentDAO.cs
@@ -29,6 +29,53 @@ namespace SkinCareDAO
                 return instance;
             }
         }
+
+
+        public Appointment GetOne(string id)
+        {
+            return _dbContext.Appointments
+                .SingleOrDefault(a => a.Id.Equals(id));
+        }
+
+        public List<Appointment> GetAll()
+        {
+            return _dbContext.Appointments
+               
+                .ToList();
+        }
+
+        public void Add(Appointment a)
+        {
+            Appointment cur = GetOne(a.Id);
+            if (cur != null)
+            {
+                throw new Exception();
+            }
+            _dbContext.Appointments.Add(a);
+            _dbContext.SaveChanges();
+        }
+
+        public void Update(Appointment a)
+        {
+            Appointment cur = GetOne(a.Id);
+            if (cur == null)
+            {
+                throw new Exception();
+            }
+            _dbContext.Entry(cur).CurrentValues.SetValues(a);
+            _dbContext.SaveChanges();
+        }
+
+        public void Delete(string id)
+        {
+            Appointment cur = GetOne(id);
+            if (cur != null)
+            {
+                _dbContext.Appointments.Remove(cur);
+                _dbContext.SaveChanges(); // Delete the object
+            }
+        }
+
         public int[] GetDashBoardAppointment(int year)
         {
             int[] monthlyAppointments = new int[12];
