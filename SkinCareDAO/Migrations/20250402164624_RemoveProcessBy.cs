bc5853707a5293d77f17b1fb9d9d41b21a0e81dd
ThinhPhoenix <laithinh1310@gmail.com>
2025-04-02 23:58:07 +0700

    migration db remove processby

---
+++ SkinCareDAO/Migrations/20250402164624_RemoveProcessBy.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SkinCareDAO.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveProcessBy : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Payments_Staffs_ProcessedBy",
+                table: "Payments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Payments_ProcessedBy",
+                table: "Payments");
+
+            migrationBuilder.DropColumn(
+                name: "ProcessedBy",
+                table: "Payments");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProcessedBy",
+                table: "Payments",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Payments_ProcessedBy",
+                table: "Payments",
+                column: "ProcessedBy");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Payments_Staffs_ProcessedBy",
+                table: "Payments",
+                column: "ProcessedBy",
+                principalTable: "Staffs",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
