680d0e862c9cb39a8826e9ed43e261ccec70b5b2
Phrimp <thaiphienn@gmail.com>
2025-04-03 11:47:41 +0700

    logging for repos

--- SkinCareRepository/CustomerRepository.cs
+++ SkinCareRepository/CustomerRepository.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SkinCareBussinessObject;
 using SkinCareDAO;
+using SkinCareDAO.Utils;
 
 namespace SkinCareRepository
 {
@@ -13,37 +14,55 @@ namespace SkinCareRepository
     {
         public Customer GetOne(string id)
         {
-            return CustomerDAO.Instance.GetOne(id);
+            LogHelper.LogInfo($"CustomerRepository.GetOne - Retrieving customer with ID: {id}");
+            var result = CustomerDAO.Instance.GetOne(id);
+            LogHelper.LogInfo($"CustomerRepository.GetOne - Retrieved customer: {(result != null ? "Success" : "Not Found")}");
+            return result;
         }
 
         public List<Customer> GetAll()
         {
-            return CustomerDAO.Instance.GetAll();
+            LogHelper.LogInfo("CustomerRepository.GetAll - Retrieving all customers");
+            var result = CustomerDAO.Instance.GetAll();
+            LogHelper.LogInfo($"CustomerRepository.GetAll - Retrieved {result.Count} customers");
+            return result;
         }
 
         public Customer GetCustomerByUserId(string userId)
         {
-            return CustomerDAO.Instance.GetCustomerByUserId(userId);
+            LogHelper.LogInfo($"CustomerRepository.GetCustomerByUserId - Retrieving customer by user ID: {userId}");
+            var result = CustomerDAO.Instance.GetCustomerByUserId(userId);
+            LogHelper.LogInfo($"CustomerRepository.GetCustomerByUserId - Retrieved customer: {(result != null ? "Success" : "Not Found")}");
+            return result;
         }
 
         public void Add(Customer a)
         {
+            LogHelper.LogInfo($"CustomerRepository.Add - Adding new customer: {a.FirstName} {a.LastName}");
             CustomerDAO.Instance.Add(a);
+            LogHelper.LogInfo($"CustomerRepository.Add - Successfully added customer with ID: {a.Id}");
         }
 
         public void Update(Customer a)
         {
+            LogHelper.LogInfo($"CustomerRepository.Update - Updating customer with ID: {a.Id}");
             CustomerDAO.Instance.Update(a);
+            LogHelper.LogInfo($"CustomerRepository.Update - Successfully updated customer");
         }
 
         public void Delete(string id)
         {
+            LogHelper.LogInfo($"CustomerRepository.Delete - Deleting customer with ID: {id}");
             CustomerDAO.Instance.Delete(id);
+            LogHelper.LogInfo($"CustomerRepository.Delete - Customer deletion processed");
         }
 
         public int[] GetDashBoardCustomer(int year)
         {
-            return CustomerDAO.Instance.GetDashBoardCustomer(year);
+            LogHelper.LogInfo($"CustomerRepository.GetDashBoardCustomer - Getting dashboard data for year: {year}");
+            var result = CustomerDAO.Instance.GetDashBoardCustomer(year);
+            LogHelper.LogInfo($"CustomerRepository.GetDashBoardCustomer - Successfully retrieved dashboard data");
+            return result;
         }
     }
 }
--- SkinCareRepository/TherapistRepository.cs
+++ SkinCareRepository/TherapistRepository.cs
@@ -6,17 +6,26 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SkinCareBussinessObject;
 using SkinCareDAO;
+using SkinCareDAO.Utils;
 
 namespace SkinCareRepository
 {
     public class TherapistRepository : ITherapistRepository
     {
-        public Task<List<Therapist>> GetAllAsync()
+        public async Task<List<Therapist>> GetAllAsync()
         {
+            LogHelper.LogInfo("TherapistRepository.GetAllAsync - Retrieving all therapists");
             var result = TherapistDAO.Instance.GetAll();
-            return Task.FromResult(result);
+            LogHelper.LogInfo($"TherapistRepository.GetAllAsync - Retrieved {result.Count} therapists");
+            return await Task.FromResult(result);
         }
 
-        public Therapist GetOne(string id) => TherapistDAO.Instance.GetOne(id); 
+        public Therapist GetOne(string id)
+        {
+            LogHelper.LogInfo($"TherapistRepository.GetOne - Retrieving therapist with ID: {id}");
+            var result = TherapistDAO.Instance.GetOne(id);
+            LogHelper.LogInfo($"TherapistRepository.GetOne - Retrieved therapist: {(result != null ? "Success" : "Not Found")}");
+            return result;
+        }
     }
-} 
+}
